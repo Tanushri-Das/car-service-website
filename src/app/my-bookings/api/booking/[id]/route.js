54d8f6242bd48d48c26b1333cda017ce4b2a6b07
tanu <dastanushri402@gmail.com>
2024-08-16 14:50:47 +0600

    implement booking data update functionality

---
+++ src/app/my-bookings/api/booking/[id]/route.js
@@ -0,0 +1,59 @@
+import { connectDB } from "@/lib/connectDB";
+import { ObjectId } from "mongodb";
+
+export const DELETE = async (request, { params }) => {
+  const db = await connectDB();
+  const bookingsCollection = await db.collection("bookings");
+  try {
+    const res = await bookingsCollection.deleteOne({
+      _id: new ObjectId(params.id),
+    });
+    return Response.json({
+      message: "deleted booking successfully",
+      response: res,
+    });
+  } catch (error) {
+    return Response.json({ message: "Something went wrong" });
+  }
+};
+
+export const PATCH = async (req, { params }) => {
+  const db = await connectDB();
+  const bookingsCollection = db.collection("bookings");
+  const updateDoc = await req.json();
+  try {
+    const res = await bookingsCollection.updateOne(
+      { _id: new ObjectId(params.id) },
+      {
+        $set: {
+          ...updateDoc,
+        },
+      },
+      {
+        upsert: true,
+      }
+    );
+    return Response.json({
+      message: "Updated bookings successfully",
+      response: res,
+    });
+  } catch (error) {
+    return Response.json({ message: "Something went wrong" });
+  }
+};
+
+export const GET = async (request, { params }) => {
+  const db = await connectDB();
+  const bookingsCollection = await db.collection("bookings");
+  try {
+    const res = await bookingsCollection.findOne({
+      _id: new ObjectId(params.id),
+    });
+    return Response.json({
+      message: "booking found",
+      data: res,
+    });
+  } catch (error) {
+    return Response.json({ message: "Something went wrong" });
+  }
+};
