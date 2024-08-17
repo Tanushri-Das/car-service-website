5243ae29132542512dc2324dd655d05ab04dd832
tanu <dastanushri402@gmail.com>
2024-08-17 09:11:21 +0600

    add basepath in auth provider

--- src/services/AuthProvider.jsx
+++ src/services/AuthProvider.jsx
@@ -3,7 +3,7 @@ import { SessionProvider } from "next-auth/react";
 import React from "react";
 
 const AuthProvider = ({ children }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <SessionProvider basePath="/api/auth">{children}</SessionProvider>;
 };
 
 export default AuthProvider;
