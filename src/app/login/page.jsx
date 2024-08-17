fb7ea98195a3e1f3b4234a793ee33fc5b0c5ea8a
tanu <dastanushri402@gmail.com>
2024-08-17 08:36:18 +0600

    again add suspense in login and signup page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -89,9 +89,8 @@
 
 // export default LoginPage;
 
-
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import SocialSignin from "@/components/shared/SocialSignin";
 import Image from "next/image";
 import Link from "next/link";
@@ -121,68 +120,69 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="m-10 md:m-20">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
-        <div className="hidden lg:block">
-          <Image
-            src="/assets/images/login/login.svg"
-            width="460"
-            height="502"
-            alt="login-img"
-          />
-        </div>
-        <div className="border w-full max-w-xl mx-auto flex flex-col items-center justify-center p-8">
-          <h2 className="font-semibold text-[40px] mb-4 text-center">
-            Login
-          </h2>
-          <form onSubmit={handleLogin} className="w-full max-w-md">
-            <div className="mb-3">
-              <label
-                htmlFor="email"
-                className="block font-semibold text-[18px]"
-              >
-                Email
-              </label>
-              <input
-                type="text"
-                name="email"
-                placeholder="Email"
-                className="input input-bordered w-full mt-2"
-              />
-            </div>
-            <div className="mb-5">
-              <label
-                htmlFor="password"
-                className="block font-semibold text-[18px]"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                placeholder="Password"
-                className="input input-bordered w-full mt-2"
-              />
-            </div>
-            <button className="btn btn-primary font-semibold text-[20px] text-white w-full">
-              Sign In
-            </button>
-            <h3 className="text-[#444444] text-center my-5">
-              Or Sign In with
-            </h3>
-            <SocialSignin />
-            <p className="text-lg text-[#737373] text-center">
-              Have an account?{" "}
-              <Link href="/signup" className="text-primary">
-                Sign up
-              </Link>
-            </p>
-          </form>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="m-10 md:m-20">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
+          <div className="hidden lg:block">
+            <Image
+              src="/assets/images/login/login.svg"
+              width="460"
+              height="502"
+              alt="login-img"
+            />
+          </div>
+          <div className="border w-full max-w-xl mx-auto flex flex-col items-center justify-center p-8">
+            <h2 className="font-semibold text-[40px] mb-4 text-center">
+              Login
+            </h2>
+            <form onSubmit={handleLogin} className="w-full max-w-md">
+              <div className="mb-3">
+                <label
+                  htmlFor="email"
+                  className="block font-semibold text-[18px]"
+                >
+                  Email
+                </label>
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="Email"
+                  className="input input-bordered w-full mt-2"
+                />
+              </div>
+              <div className="mb-5">
+                <label
+                  htmlFor="password"
+                  className="block font-semibold text-[18px]"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  className="input input-bordered w-full mt-2"
+                />
+              </div>
+              <button className="btn btn-primary font-semibold text-[20px] text-white w-full">
+                Sign In
+              </button>
+              <h3 className="text-[#444444] text-center my-5">
+                Or Sign In with
+              </h3>
+              <SocialSignin />
+              <p className="text-lg text-[#737373] text-center">
+                Have an account?{" "}
+                <Link href="/signup" className="text-primary">
+                  Sign up
+                </Link>
+              </p>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
 export default LoginPage;
-
